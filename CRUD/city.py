7cee64d2047e6a90119385d5fdf7a2dcc774afae
SHOBHIT <shobhit.roshan@gmail.com>
2020-09-17 01:07:33 +0530

    Added admin and city GUI

--- CRUD/city.py
+++ CRUD/city.py
@@ -4,58 +4,147 @@ from tkinter import messagebox
 import mysql.connector as mysql
 
 def crud_city():
+    con = mysql.connect(
+    host="localhost",
+    user="root",
+    password="shobhit2000@",
+    database="FMS"
+    )
+    cursor = con.cursor()
+
+    # old_city_code = ""
+
+    def view_city():
+
+        cursor = con.cursor()
+        cursor.execute("SELECT *FROM CITY;")
+        records = cursor.fetchall()
+        city_list.delete(0, tk.END)
+        for rec in records:
+            city_list.insert(tk.END, rec)
+
+    def get_selected_row(event):
+        global old_city_code
+        print(city_list.curselection())
+        index = city_list.curselection()[0]
+        selected_tuple = city_list.get(index)
+        print(selected_tuple)
+        city_code.delete(0, tk.END)
+        city_code.insert(tk.END,selected_tuple[0])
+        old_city_code= city_code.get()
+        print(old_city_code)
+        city.delete(0, tk.END)
+        city.insert(tk.END, selected_tuple[1])
+        zip_code.delete(0, tk.END)
+        zip_code.insert(tk.END, selected_tuple[2])
+        airport.delete(0, tk.END)
+        airport.insert(tk.END, selected_tuple[3])
+        
 
     def insert_city():
         city1 = city.get()
         city_code1 = city_code.get()
         zip_code1 = zip_code.get()
-        boarding_point1 = boarding_point.get()
+        airport1 = airport.get()
 
-        con = mysql.connect(
-                host="localhost",
-                user="root",
-                password="testpassword",
-                database="TMS"
-            )
-        cursor = con.cursor()
-        cursor.execute("INSERT INTO CITY VALUES(%s, %s, %s, %s);", [city_code1, city1, int(zip_code1), boarding_point1])
-        cursor.execute("commit")
-        cursor.close()
-        con.close()
-        City.destroy()
-        messagebox.showinfo("Request successful", "Successfully added city.")
+        if(city1 == '' or city_code1 == '' or zip_code1 == '' or airport1 == ''):
+            messagebox.showwarning("Invalid request", "Please make sure you have filled all the fields.")
+        elif (len(str(zip_code1)) != 6):
+            messagebox.showwarning("Invalid request", "Enter a valid Zip Code.")
+        else:
+            args = cursor.callproc("CHECK_IF_CITY_EXISTS", [city_code1, None])
+            if (args[-1] == 1):
+                messagebox.showwarning("City already exists", "Please enter a new city.")
+            else:
+                cursor.callproc("INSERT_CITY_DETAILS", [city_code1, city1, int(zip_code1), airport1])
+                cursor.execute("commit")
+                messagebox.showinfo("Request successful", "Successfully added city.")
+    
+    def update_city():
+        city1 = city.get()
+        city_code1 = city_code.get()
+        zip_code1 = zip_code.get()
+        airport1 = airport.get()
+        print(old_city_code)
+
+        if(city1 == '' or city_code1 == '' or zip_code1 == '' or airport1 == ''):
+            messagebox.showwarning("Invalid request", "Please make sure you have fetched all the fields.")
+        elif (len(str(zip_code1)) != 6):
+            messagebox.showwarning("Invalid request", "Enter a valid Zip Code.")
+        else:
+            cursor.callproc("UPDATE_CITY_DETAILS", [old_city_code, city_code1, city1, zip_code1, airport1])
+            cursor.execute("commit")
+            messagebox.showinfo("Request successful", "Successfully updated city.")
+
+    def delete_city():
+        if (city == '' or city_code == '' or zip_code == '' or airport == ''):
+            messagebox.showwarning("Invalid request", "Please make sure you have selected a city to delete.")
+        else:
+            cursor.callproc("DELETE_CITY_BY_CODE", [old_city_code])
+            cursor.execute("commit")
+            messagebox.showinfo("Request successful", "Successfully deleted city.")
+
+    def search_city():
+        city1 = city.get()
+        city_code1 = city_code.get()
+        zip_code1 = zip_code.get()
+        airport1 = airport.get()
+        cursor.execute("SELECT * FROM CITY WHERE CITY_CODE = %s OR CITY_NAME = %s OR ZIPCODE = %s OR AIRPORT = %s;", [city_code1, city1, zip_code1, airport1])
+        records = cursor.fetchall()
+        city_list.delete(0, tk.END)
+        for rec in records:
+            city_list.insert(tk.END, rec)
+
 
     City = tk.Tk()
     City.resizable(height = False, width = False)
     City.title('Travel Management System')
     City.geometry('720x500')
 
-    tk.Label(City, text="City", font=('Helvetica', '25')).grid(column=0, row=0, columnspan=2)
-    tk.Label(City, text="City name").grid(column=0, row=1)
-    tk.Label(City, text="City Code").grid(column=0, row=2)
-    tk.Label(City, text="Zip Code").grid(column=0, row=3)
-    tk.Label(City, text="Boarding Point").grid(column=0, row=4)
+    #tk.Label(City, text="City", font=('Helvetica', '25')).grid(column=0, row=0, columnspan=2)
+    tk.Label(City, text="City name").grid(row = 0, column = 0)
+    tk.Label(City, text="City Code").grid(row = 1, column = 0)
+    tk.Label(City, text="Zip Code").grid(row = 0, column = 2)
+    tk.Label(City, text="Airport").grid(row = 1, column = 2)
 
 
 
     city = tk.ttk.Entry(City)
-    city.grid(column=1, row=1)
+    city.grid(row = 0, column = 1)
     city_code = tk.ttk.Entry(City)
-    city_code.grid(column=1, row=2)
+    city_code.grid(row = 1, column = 1)
     zip_code = tk.ttk.Entry(City)
-    zip_code.grid(column=1, row=3)
-    boarding_point = tk.ttk.Entry(City)
-    boarding_point.grid(column=1, row=4)
+    zip_code.grid(row = 0, column = 3)
+    airport = tk.ttk.Entry(City)
+    airport.grid(row = 1, column = 3)
 
 
-    tk.ttk.Button(City, text="Submit", command=insert_city).grid(column=0, row=13, columnspan=2)
+    city_list = tk.Listbox(City, height = 15, width = 50)
+    city_list.grid(row = 2, column = 0, columnspan = 2, rowspan = 5)
 
-    # Makes the widgets responsive and centered
-    n_rows = 20
-    n_columns = 2
+    city_list.bind('<<ListboxSelect>>', get_selected_row)
+
+    sb1= tk.Scrollbar(City, width = 10)
+    sb1.grid(row=2, column=1, columnspan = 2, rowspan = 5)
+
+    city_list.configure(yscrollcommand = sb1.set)
+    sb1.configure(command = city_list.yview)
+
+
+    tk.ttk.Button(City, text="Insert", command=insert_city, width = 12).grid(row= 2, column=3)
+    tk.ttk.Button(City, text="Update", command=update_city, width = 12).grid(row= 3, column=3)
+    tk.ttk.Button(City, text="Delete", command=delete_city, width = 12).grid(row= 4, column=3)
+    tk.ttk.Button(City, text="View All", command=view_city, width = 12).grid(row= 5, column=3)
+    tk.ttk.Button(City, text="Search", command=search_city, width = 12).grid(row= 6, column=3)
+
+    #Makes the widgets responsive and centered
+    n_rows = 10
+    n_columns = 5
     for i in range(n_rows):
         City.grid_rowconfigure(i,  weight =1)
     for i in range(n_columns):
         City.grid_columnconfigure(i,  weight =1)
 
     City.mainloop()
+crud_city()
+
--- admin.py
+++ admin.py
@@ -1,70 +1,127 @@
 import tkinter as tk
 from tkinter import ttk
 from functools import partial
-from CRUD import flight, company, amenities, bus, car, city, employee, route
+from tkinter.font import Font
+from tkinter import messagebox
+import datetime
+from CRUD import flight, company, city, route
 
 def admin_screen(admin_id, admin_name):
 
-
-    def create_query(num):
-
-        if (num == 0):
-            bus.crud_bus()
-        elif (num == 1):
-            car.crud_car()
-        elif (num == 2):
-            flight.crud_flight()
-        elif (num == 3):
+    def insert():
+        entity_name = entity_names.get()
+        if (entity_name == 'City'):
             city.crud_city()
-        elif (num == 4):
+        elif (entity_name == 'Route'):
             route.crud_route()
-        elif (num == 5):
-            amenities.crud_amenities()
-        elif (num == 6):
+        elif (entity_name == 'Company'):
             company.crud_company()
-        elif (num == 7):
-            employee.crud_employee()
+        elif (entity_name == 'Flight'):
+            flight.crud_flight()
+        elif (entity_name == 'Journey'):
+            journey.crud_journey()
 
-    def update_query(num):
-        print(table_name[num]+" Update")
+    def update():
+        entity_name = entity_names.get()
+        if (entity_name == 'City'):
+            city.crud_city()
+        elif (entity_name == 'Route'):
+            route.crud_route()
+        elif (entity_name == 'Company'):
+            company.crud_company()
+        elif (entity_name == 'Flight'):
+            flight.crud_flight()
+        elif (entity_name == 'Journey'):
+            journey.crud_journey()
+        
+    def delete():    
+        entity_name = entity_names.get()
+        if (entity_name == 'City'):
+            city.crud_city()
+        elif (entity_name == 'Route'):
+            route.crud_route()
+        elif (entity_name == 'Company'):
+            company.crud_company()
+        elif (entity_name == 'Flight'):
+            flight.crud_flight()
+        elif (entity_name == 'Journey'):
+            journey.crud_journey()
 
-    def delete_query(num):
-        print(table_name[num]+" Delete")
+    def view_data():
+        entity_name = entity_names.get()
+        if (entity_name == 'City'):
+            city.crud_city()
+        elif (entity_name == 'Route'):
+            route.crud_route()
+        elif (entity_name == 'Company'):
+            company.crud_company()
+        elif (entity_name == 'Flight'):
+            flight.crud_flight()
+        elif (entity_name == 'Journey'):
+            journey.crud_journey()
 
+    
     admin = tk.Tk()
     admin.resizable(height = False, width = False)
-    admin.title('Travel Management System')
-    admin.geometry('720x500')
+    admin.title('Flight Management System')
+    admin.geometry('720x420')
+    adam = Font(family="ADAM.CG PRO", size=20)
+
+    curr_date = datetime.date.today()
+    curr_date = curr_date.strftime("%d-%m-%Y")
+
+    label1 = tk.Label(admin, text="Admin Panel", font= adam)
+    label1.place(x=280, y=40)
+    label2 = tk.Label(admin, text="Welcome Natik", font= adam)
+    label2.place(x=270, y=100)
+    label3 = tk.Label(admin, text=f"Today: {curr_date}")
+    label3.place(x=605, y=10)
 
 
-    tk.Label(admin, text="Admin Panel", font=('Helvetica', '25')).grid(column=1, row=0, columnspan=2)
+    City_button= tk.ttk.Button(admin, text="City", command=insert)
+    City_button.place(x=20, y=250, width = 100, height = 30)
 
+    Route_button= tk.ttk.Button(admin, text="Route", command=update)
+    Route_button.place(x=160, y=250, width = 100, height = 30)
 
-    table_name = ['BUS', 'CAR', 'FLIGHT', 'CITY', 'ROUTE', 'AMENITIES', 'COMPANY', 'EMPLOYEE']
-    row_counter = 4
-    add_row = 0
-    i = -1
-    counter = 0
-    while(counter<8):
-        i = (i+1)%4
-        tk.Label(admin, text=table_name[counter], font=('Helvetica', '15')).grid(column=0+i, row=1+add_row)
-        tk.ttk.Button(admin, text="Create an Entry", command=partial(create_query, counter)).grid(column=0+i, row=2+add_row)
-        tk.ttk.Button(admin, text="Update an Entry", command=partial(update_query, counter)).grid(column=0+i, row=3+add_row)
-        tk.ttk.Button(admin, text="Delete an Entry", command=partial(delete_query, counter)).grid(column=0+i, row=4+add_row)
-        row_counter-=1
-        counter+=1
+    Flight_button= tk.ttk.Button(admin, text="Flight", command=delete)
+    Flight_button.place(x=310, y=250, width = 100, height = 30)
 
-        if row_counter == 0:
-            add_row = 4
-            row_counter = 5
+    Company_button= tk.ttk.Button(admin, text="Company", command=view_data)
+    Company_button.place(x=460, y=250, width = 100, height = 30)
+
+    Journey_button = tk.ttk.Button(admin, text="Journey Details", command=view_data)
+    Journey_button.place(x=600, y=250, width = 100, height = 30)
+    
+    Update_button = tk.ttk.Button(admin, text="Update Details", command=view_data)
+    Update_button.place(x=10, y=10, width = 100, height = 30)
+    
+    # table_name = ['BUS', 'CAR', 'FLIGHT', 'CITY', 'ROUTE', 'AMENITIES', 'COMPANY', 'EMPLOYEE']
+    # row_counter = 4
+    # add_row = 0
+    # i = -1
+    # counter = 0
+    # while(counter<8):
+    #     i = (i+1)%4
+    #     tk.Label(admin, text=table_name[counter], font=('Helvetica', '15')).grid(column=0+i, row=1+add_row)
+    #     # tk.ttk.Button(admin, text="Create an Entry", command=partial(create_query, counter)).grid(column=0+i, row=2+add_row)
+    #     # tk.ttk.Button(admin, text="Update an Entry", command=partial(update_query, counter)).grid(column=0+i, row=3+add_row)
+    #     # tk.ttk.Button(admin, text="Delete an Entry", command=partial(delete_query, counter)).grid(column=0+i, row=4+add_row)
+    #     row_counter-=1
+    #     counter+=1
+
+    #     if row_counter == 0:
+    #         add_row = 4
+    #         row_counter = 5
 
     # Makes the widgets responsive and centered
-    n_rows = 15
-    n_columns = 4
-    for i in range(n_rows):
-        admin.grid_rowconfigure(i,  weight =1)
-    for i in range(n_columns):
-        admin.grid_columnconfigure(i,  weight =1)
-# 
-#     admin.mainloop()
-# admin_screen(1, 'admin')
+    # n_rows = 15
+    # n_columns = 4
+    # for i in range(n_rows):
+    #     admin.grid_rowconfigure(i,  weight =1)
+    # for i in range(n_columns):
+    #     admin.grid_columnconfigure(i,  weight =1)
+
+    admin.mainloop()
+
+admin_screen(1, 'admin')
